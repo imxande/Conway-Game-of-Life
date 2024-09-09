ea445fcfcec50b5542635c91a0d22f3ec706aa90
imxande <alerodriguezlarosa@gmail.com>
2024-09-09 00:53:31 -0400

    upadte

--- src/main.py
+++ src/main.py
@@ -1,2 +1,19 @@
+from tkinter import Tk
+
+
 def main():
-    print("Hello Wold")
+    # setup main application window
+    root = Tk()
+
+    # main window initial configurations
+    root.geometry("600x600")
+    root.wm_title("Conway\'s Game of Life")
+
+
+    # start application mainloop
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
