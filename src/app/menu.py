cdeeb0c4d1c6c6cd8d50da81b0a416c60d9d3b49
imxande <alerodriguezlarosa@gmail.com>
2024-09-14 17:07:25 -0400

    refact: History view displays history on a toplevel window

--- src/app/menu.py
+++ src/app/menu.py
@@ -1,4 +1,4 @@
-from tkinter import Menu, messagebox, Toplevel
+from tkinter import Menu, messagebox, Toplevel, Message
 
 class AppMenu:
     def __init__(self, root):
@@ -35,7 +35,19 @@ class AppMenu:
     def show_history(self):
         """
         Displays game history.
-        """ 
+        """
         self.history_view = Toplevel(self.root)
         self.history_view.title("Game History")
+
+        history_message = (
+            "The Game of Life, also known simply as Life, is a cellular automaton devised by the British mathematician John Horton Conway in 1970.\n"
+            "It is a zero-player game, meaning that its evolution is determined by its initial state, requiring no further input.\n"
+            "One interacts with the Game of Life by creating an initial configuration and observing how it evolves.\n"
+            "It is Turing complete and can simulate a universal constructor or any other Turing machine."
+        )
+        
+        # display message
+        message = Message(self.history_view, text=history_message, padx=20, pady=20)
+        message.pack()
+
 
